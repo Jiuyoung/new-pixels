2714249515b2d1134c951f7eea3e9067a6681824
jiuyoung <shuai_zhang_me@163.com>
2019-06-02 23:09:43 +0800

    增加Cros支持

--- src/main/java/edu/xidian/pixels/Config/WebMvcConfig.java
+++ src/main/java/edu/xidian/pixels/Config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -17,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * WebMvcConfig
  */
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
