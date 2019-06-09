2c31ac6d2efacb49e41b88058c665044bf19924e
jiuyoung <shuai_zhang_me@163.com>
2019-06-09 22:35:25 +0800

    缓存使用fastjson来序列化

---
+++ src/main/java/edu/xidian/pixels/Config/RedisConfig.java
@@ -0,0 +1,67 @@
+package edu.xidian.pixels.Config;
+
+import java.time.Duration;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置类
+ *
+ * @author zcc ON 2018/3/19
+ **/
+@Configuration
+@EnableCaching//开启注解
+public class RedisConfig {
+    @Bean
+    public FastJsonRedisSerializer<Object> fastJsonRedisSerializer() {
+        return new FastJsonRedisSerializer<>(Object.class);
+    }
+
+    @Bean
+    @Primary
+    //如使用注解的话需要配置cacheManager
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory, FastJsonRedisSerializer<Object> serializer) {
+        //初始化一个RedisCacheWriter
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
+        RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(serializer);
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+        //设置默认超过期时间是1天
+        defaultCacheConfig = defaultCacheConfig.entryTtl(Duration.ofMinutes(10));
+        //初始化RedisCacheManager
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
+        return cacheManager;
+    }
+
+    // 以下两种redisTemplate自由根据场景选择
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory, FastJsonRedisSerializer<Object> serializer) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setValueSerializer(serializer);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        return stringRedisTemplate;
+    }
+}
